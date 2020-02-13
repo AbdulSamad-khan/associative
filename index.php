7c2ef4d50c3dbde6e5746cee50620f1d65e1cf01
Abdul samad <studyonlinetech47@gmail.com>
2020-02-13 23:43:50 +0500

    account details

--- index.php
+++ index.php
@@ -12,9 +12,14 @@ $array = array(
   'hob1' => 'cricket',
   'hob2' => 'rod games/Football',
   'hob3' => 'programming'
+),
+'Accounts' => (
+  'Acc1Balance' => '10,000',
+  'Acc2Balance' => '40,000',
+  'Acc3Balance' => '60,000'
 )
 );
 echo "Name" . $array['name'] . "Profession" . $array['profession'] . "Age" . $array['age'] . "<br>";
 echo $array['Bachelor']['BSSE'];
-echo 
+echo
  ?>

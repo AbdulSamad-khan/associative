cb9ff251c166aa6e5d0a5124e146ddce5714d15b
Abdul samad <studyonlinetech47@gmail.com>
2020-02-13 23:37:55 +0500

    hobbies

--- index.php
+++ index.php
@@ -7,8 +7,14 @@ $array = array(
   'matriculation' => "A Grade",
   'Intermediate' => "D Grade",
   'BSSE' => "Software Engnieering"
+),
+'hobbies' => array(
+  'hob1' => 'cricket',
+  'hob2' => 'rod games/Football',
+  'hob3' => 'programming'
 )
 );
 echo "Name" . $array['name'] . "Profession" . $array['profession'] . "Age" . $array['age'] . "<br>";
 echo $array['Bachelor']['BSSE'];
+echo 
  ?>

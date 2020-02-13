868369db122ed41007aeaeb69c32a4b98106cf93
Abdul samad <studyonlinetech47@gmail.com>
2020-02-13 23:28:32 +0500

    intial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$array = array(
+'name' => "Abdul samad",
+'profession' => "Student",
+'age' => "gender",
+'Bachelor' => array (
+  'matriculation' => "A Grade",
+  'Intermediate' => "D Grade",
+  'BSSE' => "Software Engnieering"
+)
+);
+echo "Name" . $array['name'] . "Profession" . $array['profession'] . "Age" . $array['age'] . "<br>";
+echo $array['Bachelor']['BSSE'];
+ ?>
